257b7ec01315e2bc78ad271589d2f882fc65a209
anhnhph33991 <hoang838604@gmail.com>
2025-02-25 16:18:29 +0700

    chore: remove file

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- ==========Banner-Section========== -->
-    <AppBanner />
+    <!-- <AppBanner /> -->
     <!-- ==========Banner-Section========== -->
 
     <!-- ==========Ticket-Search========== -->
