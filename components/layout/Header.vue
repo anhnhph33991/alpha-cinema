1955be2aa348e4a3166b7a027e89deaf4d566caa
anhnhph33991 <hoang838604@gmail.com>
2025-02-25 19:16:06 +0700

    chore: remove comment

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,35 +1,4 @@
 <template>
-  <!-- <header :class="['header-section', { 'header-active': isActive }]">
-    <div class="container">
-      <div class="header-wrapper">
-        <div class="logo">
-          <NuxtLink :to="{ name: 'index' }">
-            <img src="/theme/img/logo/logo.png" alt="logo" />
-          </NuxtLink>
-        </div>
-        <ul class="menu">
-          <li v-for="(item, index) in navMenu" :key="index">
-            <NuxtLink :to="item.link" exact-active-class="active">
-              {{ item.title }}
-            </NuxtLink>
-          </li>
-
-          <li class="header-button pr-0" v-if="authStore.isLogin">
-            <a @click.prevent="logout">{{ authStore.user.name }}</a>
-          </li>
-          <li class="header-button pr-0" v-else>
-            <NuxtLink :to="{ name: 'login' }">Đăng Nhập</NuxtLink>
-          </li>
-        </ul>
-        <div class="header-bar d-lg-none">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    </div>
-  </header> -->
-
   <div>
     <div class="al-pre-header">
       <div class="container">
