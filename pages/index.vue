ce98c4b422149f97d1a7c9d2fd7f571a4d09bffb
anhnhph33991 <hoang838604@gmail.com>
2025-04-10 15:12:13 +0700

    fix: hidden loading index

--- pages/index.vue
+++ pages/index.vue
@@ -86,30 +86,32 @@
         </a-modal>
       </ClientOnly>
 
-      <div class="container" v-if="movieStore.movies.data">
-        <a-tabs v-model="tabActive" :default-active-key="'2'">
-          <a-tab-pane key="1" tab="Phim Sắp Chiếu">
-            <!-- <MovieList :movies="[]" /> -->
-            <MovieList :movies="movieIsUpcoming" :btnBuy="false" />
-          </a-tab-pane>
-          <a-tab-pane key="2" tab="Phim Đang Chiếu">
-            <!-- <MovieList :movies="movieStore.movies?.data || []" /> -->
-            <MovieList :movies="movieIsShowing || []" :btnBuy="true" />
-          </a-tab-pane>
-          <a-tab-pane key="3" tab="Suất Chiếu Đặc Biệt">
-            <MovieList :movies="movieIsSpecial" :btnBuy="true" />
-            <!-- <MovieList :movies="[]" /> -->
-          </a-tab-pane>
-        </a-tabs>
+      <div class="container">
+        <ClientOnly>
+          <a-tabs v-model="tabActive" :default-active-key="'2'">
+            <a-tab-pane key="1" tab="Phim Sắp Chiếu">
+              <!-- <MovieList :movies="[]" /> -->
+              <MovieList :movies="movieIsUpcoming" :btnBuy="false" />
+            </a-tab-pane>
+            <a-tab-pane key="2" tab="Phim Đang Chiếu">
+              <!-- <MovieList :movies="movieStore.movies?.data || []" /> -->
+              <MovieList :movies="movieIsShowing || []" :btnBuy="true" />
+            </a-tab-pane>
+            <a-tab-pane key="3" tab="Suất Chiếu Đặc Biệt">
+              <MovieList :movies="movieIsSpecial" :btnBuy="true" />
+              <!-- <MovieList :movies="[]" /> -->
+            </a-tab-pane>
+          </a-tabs>
+        </ClientOnly>
       </div>
 
-      <div class="container" v-else>
+      <!-- <div class="container">
         <div
           class="d-flex justify-content-center align-content-center align-items-center h-screen"
         >
           <a-spin />
         </div>
-      </div>
+      </div> -->
     </div>
     <!-- ==========Movie-Section========== -->
   </div>
