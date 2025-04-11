ac823a7469e54bca118c7e30aece59861f63b0f5
anhnhph33991 <hoang838604@gmail.com>
2025-04-11 21:03:37 +0700

    llll

--- pages/account/order/[code].vue
+++ pages/account/order/[code].vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
+  <div class="container">
     <div class="row">
-      <div class="col-lg-8">
-        <div class="card">
+      <div class="col-xl-9 col-lg-12 col-md-12">
+        <div class="card mt-2 mb-5">
           <div class="card-header">
-            <div class="d-flex justify-content-between">
+            <div class="d-flex justify-content-between p-2">
               <div class="fst-normal fw-bold">Thông tin vé</div>
               <div>
-                <span class="badge bg-primary">Chờ xác nhận</span>
+                <span class="badge bg-warning">Chờ xác nhận</span>
               </div>
             </div>
           </div>
@@ -18,13 +18,165 @@
                 <img
                   src="https://alphacinema.me/storage/movie_images/LyfJghF3fgpYG5aNNK2ndwcOmfPSgVPAMKjtPNG2.png"
                   alt="phim hay"
+                  style="width: 100%; height: 100%"
+                  class="mb-3"
                 />
               </div>
               <div class="col-lg-8">
                 <div>
                   <div class="row">
                     <div class="col-lg-12">
-                      <h1 class="text-primary">Nghi Lễ Trục Quỷ</h1>
+                      <div class="ticket-info-title text-center">
+                        <h6
+                          class="mb-4 fs-4 fw-bold"
+                          style="color: rgb(81, 86, 190)"
+                        >
+                          Nghi Lễ Trục Quỷ
+                        </h6>
+                      </div>
+                    </div>
+                    <div class="col-3 text-16">Lịch chiếu</div>
+                    <div class="col-9 text-start text-16 fw-medium">
+                      10:00 - 11:24 (12/04/2025)
+                    </div>
+                    <div class="col-3 text-16">Thời lượng</div>
+                    <div class="col-9 text-start text-16 fw-medium">84</div>
+                    <div class="col-3 text-16">Định dạng</div>
+                    <div class="col-9 text-start text-16 fw-medium">
+                      2D Lồng Tiếng
+                    </div>
+                    <div class="col-3 text-16">Phòng chiếu</div>
+                    <div class="col-9 text-start text-16 fw-medium">
+                      HN_M792
+                    </div>
+                    <div class="col-3 text-16">Thể loại</div>
+                    <div class="col-9 text-start text-16 fw-medium">Tâm lý</div>
+                    <div class="col-3 text-16">Ghế ngồi</div>
+                    <div class="col-9 text-start text-16 fw-medium">
+                      <div class="row">
+                        <span
+                          class="col-2 border border-secondary-subtle rouder-3 mx-1 mb-2 text-center"
+                          >I11</span
+                        >
+                        <span
+                          class="col-2 border border-secondary-subtle rouder-3 mx-1 mb-2 text-center"
+                          >I12</span
+                        >
+                        <span
+                          class="col-2 border border-secondary-subtle rouder-3 mx-1 mb-2 text-center"
+                          >J10</span
+                        >
+                        <span
+                          class="col-2 border border-secondary-subtle rouder-3 mx-1 mb-2 text-center"
+                          >J11</span
+                        >
+                        <span
+                          class="col-2 border border-secondary-subtle rouder-3 mx-1 mb-2 text-center"
+                          >J12</span
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr class="mt-4" />
+              <div class="col-xl-12">
+                <div class="fs-5 fw-semibold">Đồ ăn</div>
+
+                <!-- combo -->
+                <div class="row">
+                  <div class="col-md-6 mb-3">
+                    <div class="card h-100">
+                      <div class="card-body row">
+                        <div class="col-4">
+                          <img
+                            class="w-100 h-75"
+                            src="https://alphacinema.me/storage/comboImages/QpaY6X34CWEZuhn1danLRsxUe0wgIDHExU2OHvqb.png"
+                            alt="Combo ngọt ngào"
+                          />
+                        </div>
+                        <div class="col-8">
+                          <h6 class="card-title">Combo ngọt ngào</h6>
+                          <ul class="list-unstyled mb-2">
+                            <li>Bỏng ngô thường (SL: 1)</li>
+                            <li>Nước Cocacola (SL: 2)</li>
+                          </ul>
+                          <p class="mb-0">
+                            2 x 90.000 VND =
+                            <span class="price">180.000</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6 mb-3">
+                    <div class="card h-100">
+                      <div class="card-body row">
+                        <div class="col-4">
+                          <img
+                            class="w-100 h-75"
+                            src="https://alphacinema.me/storage/comboImages/RQ8UPqOcPraPy4F4fSG3T4kD6GmXSan2cR6V9Ety.png"
+                            alt="Combo Gia đình"
+                          />
+                        </div>
+                        <div class="col-8">
+                          <h6 class="card-title">Combo Gia đình</h6>
+                          <ul class="list-unstyled mb-2">
+                            <li>Bỏng ngô thường (SL: 2)</li>
+                            <li>Nước Cocacola (SL: 4)</li>
+                            <li>Khoai tây chiên (SL: 1)</li>
+                          </ul>
+                          <p class="mb-0">
+                            2 x 210.000 VND =
+                            <span class="price">420.000</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- Food -->
+                <div class="row">
+                  <div class="col-md-6 mb-3">
+                    <div class="card h-100">
+                      <div class="card-body row">
+                        <div class="col-4">
+                          <img
+                            class="w-100 h-75"
+                            src="https://alphacinema.me/storage/foodImages/9rx7OY31Z4rXHkeBRxnvDEaoco9sGZtOLtG45a6L.png"
+                            alt="Sinh tố bơ"
+                          />
+                        </div>
+                        <div class="col-8">
+                          <h6 class="card-title">Sinh tố bơ</h6>
+
+                          <p class="mb-0">
+                            2 x 50.000 VND =
+                            <span class="price">100.000</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6 mb-3">
+                    <div class="card h-100">
+                      <div class="card-body row">
+                        <div class="col-4">
+                          <img
+                            class="w-100 h-75"
+                            src="https://alphacinema.me/storage/foodImages/Hpy6jIoYMX6pFVL41N5L5e20y081dmx1fQyZJhuY.png"
+                            alt="Bánh ngọt"
+                          />
+                        </div>
+                        <div class="col-8">
+                          <h6 class="card-title">Bánh ngọt</h6>
+
+                          <p class="mb-0">
+                            3 x 25.000 VND =
+                            <span class="price">75.000</span>
+                          </p>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -34,12 +186,64 @@
         </div>
       </div>
 
-      <div class="col-lg-4">
-        <a-card title="Card title" :bordered="false" style="width: 300px">
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </a-card>
+      <div class="col-xl-3 col-lg-12 col-md-12">
+        <div class="card mt-2">
+          <div class="card-header">
+            <div class="d-flex justify-content-between p-2">
+              <div class="fst-normal fw-bold">Thông tin người đặt</div>
+            </div>
+          </div>
+
+          <div class="card-body">
+            <div class="row">
+              <div class="mb-1">
+                <span class="text-body-secondary"> Tên: </span>
+                <small class="fw-medium">Anh Nguyễn Hoàng</small>
+              </div>
+              <div class="mb-1">
+                <span class="text-body-secondary"> Email: </span>
+                <small class="fw-medium">hoang838604@gmail.com</small>
+              </div>
+              <div class="mb-1">
+                <span class="text-body-secondary"> SĐT: </span>
+                <small class="fw-medium text-danger">Chưa có</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card mt-2">
+          <div class="card-header">
+            <div class="d-flex justify-content-between p-2">
+              <div class="fst-normal fw-bold">Thông tin thanh toán</div>
+            </div>
+          </div>
+
+          <div class="card-body">
+            <div class="row">
+              <div class="mb-1">
+                <span class="text-body-secondary"> Thời gian: </span>
+                <small class="fw-medium">20:03 - 11/04/2025</small>
+              </div>
+              <div class="mb-1">
+                <span class="text-body-secondary"> Phương thức: </span>
+                <small class="fw-medium">VNPAY</small>
+              </div>
+              <div class="mb-1">
+                <span class="text-body-secondary"> Tiền Vé: </span>
+                <small class="fw-medium text-danger">292.000đ</small>
+              </div>
+              <div class="mb-1">
+                <span class="text-body-secondary"> Tiền Đồ ăn: </span>
+                <small class="fw-medium text-danger">775.000đ</small>
+              </div>
+              <div class="mb-1">
+                <span class="text-body-secondary"> Tổng tiền: </span>
+                <small class="fw-medium text-danger">1.098.360đ</small>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
