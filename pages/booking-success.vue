f53a207c990f4ab087540b1915a656cafb124ff1
anhnhph33991 <hoang838604@gmail.com>
2025-04-27 23:54:59 +0700

    fix(booking-reset): css

--- pages/booking-success.vue
+++ pages/booking-success.vue
@@ -3,7 +3,7 @@
     <div
       class="d-flex justify-content-center align-items-center align-content-center h-screen"
     >
-      <div>
+      <div class="booking-page">
         <ClientOnly>
           <a-result
             status="success"
@@ -11,12 +11,14 @@
             sub-title="Chúc bạn có một trả nghiệm đáng nhớ"
           >
             <template #extra>
-              <a-button
-                key="console"
-                type="primary"
+              <a
+                style="display: block"
+                class="btn btn-2 btn-mua-ve2 fancybox-fast-view"
                 @click="navigateTo({ name: 'index' })"
-                >Quay về trang chủ</a-button
               >
+                Quay về trang chủ
+              </a>
+
               <!-- <a-button key="buy">Buy Again</a-button> -->
             </template>
           </a-result>
@@ -29,3 +31,11 @@
 <script setup>
 import { ClientOnly } from "#components";
 </script>
+
+<style scoped>
+.booking-page
+  :where(.css-dev-only-do-not-override-1p3hq3p)[class^="ant-result"],
+:where(.css-dev-only-do-not-override-1p3hq3p)[class*=" ant-result"] {
+  font-family: "Oswald", sans-serif !important;
+}
+</style>
